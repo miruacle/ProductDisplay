0a7019f080577d44dbb09b93ffbfcf26ebe648d1
trg <trg@10.42.22.30>
2016-10-12 19:06:37 +0530

    a

--- ProductExperiment/src/com/operations/Display.java
+++ ProductExperiment/src/com/operations/Display.java
@@ -15,9 +15,5 @@ public class Display extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
 
 }
